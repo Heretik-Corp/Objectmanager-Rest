f5dfea547275d6f796bbf19ca6287a0b6633b57c
David Barnes <david.barnes@heretik.com>
2018-05-21 11:54:06 -0500

    fixed a script issue

--- tools/ilmerge/Merge-Files.ps1
+++ tools/ilmerge/Merge-Files.ps1
@@ -1,9 +1,11 @@
 
 param(
-    $Platform = "Debug"
+    $Platform = "Debug",
+    $outputDir = "..\..\..\bin\$Patform"
+
 )
 
 Push-Location ('{0}\..\..\ObjectManager.Rest\bin\{1}' -f $PSScriptRoot, $Platform)
 New-Item -ItemType Directory -Path ..\..\..\bin\$Patform -Force > $null
-..\..\..\tools\ilmerge\ILMerge.exe /wildcards /out:..\..\..\bin\$Patform\ObjectManager.Rest.dll /target:library  /lib:.\ /targetPlatform:v4 .\ObjectManager.Rest.*.dll
+..\..\..\tools\ilmerge\ILMerge.exe /wildcards /out:$outputDir\ObjectManager.Rest.dll /target:library  /lib:.\ /targetPlatform:v4 .\ObjectManager.Rest.*.dll
 Pop-Location
