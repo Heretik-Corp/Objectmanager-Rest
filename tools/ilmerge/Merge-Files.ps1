905f8cacd058bc14ec49f826bc907e61ce3d0a6e
David Barnes <david.barnes@heretik.com>
2018-05-21 13:52:04 -0500

    fixed il merge script

--- tools/ilmerge/Merge-Files.ps1
+++ tools/ilmerge/Merge-Files.ps1
@@ -6,6 +6,6 @@ param(
 )
 
 Push-Location ('{0}\..\..\ObjectManager.Rest\bin\{1}' -f $PSScriptRoot, $Platform)
-New-Item -ItemType Directory -Path ..\..\..\bin\$Patform -Force > $null
+New-Item -ItemType Directory -Path $outputDir -Force > $null
 ..\..\..\tools\ilmerge\ILMerge.exe /wildcards /out:$outputDir\ObjectManager.Rest.dll /target:library  /lib:.\ /targetPlatform:v4 .\ObjectManager.Rest.*.dll
 Pop-Location
