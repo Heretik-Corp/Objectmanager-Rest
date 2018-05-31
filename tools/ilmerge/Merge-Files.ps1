6a5e042953c75c96f4f414d34cb20ce2e700a339
Kate Samuels <kate.samuels@heretik.com>
2018-05-31 11:09:28 -0500

    Export ObjectManager.Rest.dll

--- tools/ilmerge/Merge-Files.ps1
+++ tools/ilmerge/Merge-Files.ps1
@@ -7,5 +7,5 @@ param(
 
 Push-Location ('{0}\..\..\ObjectManager.Rest\bin\{1}' -f $PSScriptRoot, $Platform)
 New-Item -ItemType Directory -Path $outputDir -Force > $null
-..\..\..\tools\ilmerge\ILMerge.exe /wildcards /out:$outputDir\ObjectManager.Rest.dll /target:library  /lib:.\ /targetPlatform:v4 .\ObjectManager.Rest.*.dll
+..\..\..\tools\ilmerge\ILMerge.exe /wildcards /out:$outputDir\ObjectManager.Rest.dll /target:library  /lib:.\ /targetPlatform:v4 .\ObjectManager.Rest.*.dll ObjectManager.Rest.dll
 Pop-Location
