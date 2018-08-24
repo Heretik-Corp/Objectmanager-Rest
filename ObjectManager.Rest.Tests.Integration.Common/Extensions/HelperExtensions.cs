b04e6af3b9186253b0ff3898a8c4a1f8151d6aa4
dvbarnes <dvbarnes89@gmail.com>
2018-08-24 15:18:38 -0500

    Single multi user fields (#23)
    * Added single object, multi object, user support
    
    * added last test cases for single/multi/user support

--- ObjectManager.Rest.Tests.Integration.Common/Extensions/HelperExtensions.cs
+++ ObjectManager.Rest.Tests.Integration.Common/Extensions/HelperExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using kCura.Relativity.Client;
 using Relativity.API;
 
 namespace ObjectManager.Rest.Tests.Integration.Common.Extensions
@@ -10,6 +11,14 @@ namespace ObjectManager.Rest.Tests.Integration.Common.Extensions
         {
             return helper.GetServicesManager().GetRESTServiceUrl().GetLeftPart(UriPartial.Authority);
         }
+
+        public static IRSAPIClient GetRSAPIClient(this IHelper helper, int workspaceId, ExecutionIdentity identity = ExecutionIdentity.System)
+        {
+            var client = helper.GetServicesManager().CreateProxy<IRSAPIClient>(identity);
+            client.APIOptions.WorkspaceID = workspaceId;
+            return client;
+        }
+
         public static string GetArtifactName(this IHelper helper, int workspaceId, Guid artifactGuid)
         {
             var sql = @"select TextIDentifier from eddsdbo.Artifact a
